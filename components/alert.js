b2615c8caacbe4bd893419a10564cf3753a5f1d2
micatalyst <prof.miguelteixeira@gmail.com>
2022-10-30 19:10:02 +0000

    update na instalação do lefthook

--- components/alert.js
+++ components/alert.js
@@ -1,39 +1,37 @@
-import Container from './container'
-import cn from 'classnames'
-import Link from 'next/link'
-import { EXAMPLE_PATH } from '../lib/constants'
+import Container from "./container";
+import cn from "classnames";
+import Link from "next/link";
+import { EXAMPLE_PATH } from "../lib/constants";
 
 export default function Alert({ preview }) {
   return (
     <div
-      className={cn('border-b', {
-        'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
+      className={cn("border-b", {
+        "bg-accent-7 border-accent-7 text-white": preview,
+        "bg-accent-1 border-accent-2": !preview,
       })}
     >
       <Container>
         <div className="py-2 text-center text-sm">
           {preview ? (
             <>
-              This is page is a preview.{' '}
+              This is page is a preview.{" "}
               <Link href="/api/exit-preview">
-                  <a
-                    className="underline hover:text-cyan duration-200 transition-colors"
-                  >
-                    Click here
-                  </a>{' '}
+                <a className="underline hover:text-cyan duration-200 transition-colors">
+                  Click here
+                </a>{" "}
               </Link>
               to exit preview mode.
             </>
           ) : (
             <>
-              The source code for this blog is{' '}
-              <Link href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}>
-                  <a
-                    className="underline hover:text-success duration-200 transition-colors"
-                  >
-                    available on GitHub
-                  </a>
+              The source code for this blog is{" "}
+              <Link
+                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              >
+                <a className="underline hover:text-success duration-200 transition-colors">
+                  available on GitHub
+                </a>
               </Link>
               .
             </>
@@ -41,5 +39,5 @@ export default function Alert({ preview }) {
         </div>
       </Container>
     </div>
-  )
+  );
 }
