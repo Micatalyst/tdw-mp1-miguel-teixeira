89a7d73fe86529ebac5687d0a6ab1a204d91a635
micatalyst <prof.miguelteixeira@gmail.com>
2022-10-31 18:51:48 +0000

    contentful e versão do next.js atualizada

--- components/alert.js
+++ components/alert.js
@@ -26,10 +26,7 @@ export default function Alert({ preview }) {
           ) : (
             <>
               The source code for this blog is{" "}
-              <Link
-                legacyBehavior
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              >
+              <Link legacyBehavior href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}>
                 <a className="underline hover:text-success duration-200 transition-colors">
                   available on GitHub
                 </a>
